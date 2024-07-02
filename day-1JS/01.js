bfdcfaffdb340d1c3dc734f9046c12d9bd0a178d
mohammad <mohammad.green13@gmail.com>
2024-07-03 00:29:59 +0600

    Add files via upload

---
+++ day-1JS/01.js
@@ -0,0 +1,124 @@
+// Variables//
+
+var name1 = "mohammad"
+var name2 = "nabiha"
+var name3 = "nazia"
+console.log(name2);
+
+
+let city = "dhaka"
+let country = "bangladesh"
+console.log(country);
+
+
+const a = 10
+const b = 20
+const c = 30
+console.log( a + b + c);
+
+
+// Data types//
+
+// 1-number
+let x1 = 40
+let y1 = 30.44 
+console.log(typeof(y1)); 
+
+// 2-null
+let x = null
+console.log(typeof(x));
+
+// 3-undefined
+let myName;
+console.log(typeof(myName));
+
+// 4-Boolean
+let heyThere = true
+console.log(typeof(heyThere));
+
+
+// 5-String
+let car = "";
+console.log(typeof(car));
+
+
+// 6-Symbol
+const sym = Symbol("foo");
+console.log(typeof(sym));
+
+// 7-BigInt
+let number = BigInt("999999999999999");
+console.log(typeof(number));
+
+
+// type convertion and coercion
+
+let z = true
+console.log("Number : " + Number(z));
+console.log("String : " + String(z)); 
+console.log( "Boolean: " + Boolean(z)); 
+
+
+// operators//
+
+// Arithmetic Operators
+let o = 3;
+let p = (40 + 90) / a;
+console.log(p);
+
+let oi = 3;
+let pi = (40 + 90) * a;
+console.log(pi);
+
+// Assignment Operator
+var xi = 100;
+xi += 50;
+console.log(xi);
+
+let mi = 8;
+mi **= 6;
+console.log(mi);
+
+
+let yi = 90;
+yi %= 30;
+console.log(yi);
+
+let ki = 100;
+ki -= 50;
+console.log(ki);
+
+
+
+// Comparison Operator
+let gi = 59;
+console.log(gi == 59);
+
+let fi = 59;
+console.log(fi > 59);
+
+
+
+// logical opertor
+let ei= 69;
+let ri = 39;
+console.log(ei < 100 && ri > 35);
+
+
+let ii = 6;
+let oo = 2;
+console.log(ii == 6 || oo == 5);
+console.log(ii == 5 || oo == 5);
+
+let iu = 6;
+let ou = 3;
+console.log(!iu === ou);
+console.log(!iu > ou);
+
+
+
+
+
+
+
+
